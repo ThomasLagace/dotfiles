84139797179c8029c4cb2af1baab5e6dac242dd5
Thomas Lagace <ThomasLagace@users.noreply.github.com>
2018-07-13 23:34:19 -0400

    Laptop polybar changes

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Kill polybar if it's running
 killall -q polybar
 # Wait for it to shut down
@@ -7,6 +9,6 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 if [[ "$(hostname)" == 'Tower' ]]; then 
     polybar -r desktop1 &
     polybar -r desktop2 &
-elif [[ "$(hostname)" == 'Tome' ]]; then
+elif [[ "$(hostname)" == 'tome' ]]; then
     polybar -r laptop &
 fi
